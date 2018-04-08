e775509dfaf5953c7c98c1f66ca3d9d5edeb25da
Mark Nielsen <nielsen.markus11@gmail.com>
2018-04-08 00:03:44 -0600

    Add full code to connect.

--- postgresql-linux.R
+++ postgresql-linux.R
@@ -1,5 +1,21 @@
-install.packages("RJDBC")
+# I'm using 
 
-library(RJDBC)
+# sudo apt-get install libpq-dev
+install.packages("RPostgreSQL")
+
+library(RPostgreSQL)
 library(DBI)
+library(getPass)
 
+pgdrv <- DBI::dbDriver(drvName = "PostgreSQL")
+
+db <-DBI::dbConnect(pgdrv, dbname="postgres",
+                      host="localhost", port=5432,
+                      user = 'postgres',
+                      password = getPass("Enter Password:"))
+
+library(dplyr)
+# Write to database
+copy_to(db,mtcars,"mtcars", overwrite=TRUE)
+
+DBI::dbGetQuery(db,"SELECT * FROM MTCARS")
