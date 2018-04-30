dd98325e40368c62a15310cc007e595cb70529f2
Mark Nielsen <nielsen.markus11@gmail.com>
2018-04-29 22:59:33 -0600

    Finalize PostgreSQL example.

--- postgresql-linux.R
+++ postgresql-linux.R
@@ -1,21 +1,61 @@
-# I'm using 
+### RPostgreSQL setup ###
 
 # sudo apt-get install libpq-dev
-install.packages("RPostgreSQL")
+# install.packages("RPostgreSQL")
 
 library(RPostgreSQL)
-library(DBI)
 library(getPass)
 
 pgdrv <- DBI::dbDriver(drvName = "PostgreSQL")
 
-db <-DBI::dbConnect(pgdrv, dbname="postgres",
-                      host="localhost", port=5432,
-                      user = 'postgres',
-                      password = getPass("Enter Password:"))
+db <-DBI::dbConnect(pgdrv,
+                    dbname="postgres",
+                    host="localhost", port=5432,
+                    user = 'postgres',
+                    password = getPass("Enter Password:"))
 
-library(dplyr)
 # Write to database
-copy_to(db,mtcars,"mtcars", overwrite=TRUE)
+DBI::dbWriteTable(db, "mtcars", mtcars)
+
+DBI::dbDisconnect(db)
+
+
+### odbc setup ###
+
+# # Install the unixODBC library
+# apt-get install unixodbc unixodbc-dev
+# 
+# # PostgreSQL ODBC Drivers
+# apt-get install odbc-postgresql
+
+# Set up connection with connection string
+db <- DBI::dbConnect(odbc::odbc(),
+                     Driver = "PostgreSQL Unicode",
+                     Database = "postgres",
+                     UserName = "postgres",
+                     Password = getPass("Enter Password:"),
+                     Servername = "localhost",
+                     Port = 5432)
+
+# Set up connection with DSN
+# - Modify /etc/odbc.ini 
+db <- dbConnect(odbc::odbc(), "PostgreSQL")
 
 DBI::dbGetQuery(db,"SELECT * FROM MTCARS")
+
+
+DBI::dbDisconnect(db)
+
+
+### RJDBC setup ###
+# install.packages('RJDBC')
+library(RJDBC)
+
+db <- DBI::dbConnect(RJDBC::JDBC("org.postgresql.Driver","postgresql-42.2.2.jar"),
+               url = "jdbc:postgresql://localhost:5432/postgres",
+               user = "postgres",
+               password = getPass("Enter Password:"))
+
+DBI::dbGetQuery(db,"SELECT * FROM MTCARS")
+
+DBI::dbDisconnect(db)
