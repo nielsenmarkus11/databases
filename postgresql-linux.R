a37521dbb3a8fd5908d1dccb7b846027bc30e429
Mark Nielsen <nielsen.markus11@gmail.com>
2018-05-07 21:06:51 -0600

    Clean up formatting and add JDBC connection to sqlserver-windows.r

--- postgresql-linux.R
+++ postgresql-linux.R
@@ -10,13 +10,18 @@ pgdrv <- dbDriver(drvName = "PostgreSQL")
 
 db <-DBI::dbConnect(pgdrv,
                     dbname="postgres",
-                    host="localhost", port=5432,
+                    host="localhost", 
+                    port=5432,
                     user = 'postgres',
                     password = getPass("Enter Password:"))
 
 # Write to database
 DBI::dbWriteTable(db, "mtcars", mtcars)
 
+# Execute SQL query
+DBI::dbGetQuery(db,"SELECT * FROM MTCARS")
+
+#Disconnect from database
 DBI::dbDisconnect(db)
 
 
@@ -28,34 +33,49 @@ DBI::dbDisconnect(db)
 # # PostgreSQL ODBC Drivers
 # apt-get install odbc-postgresql
 
-# Set up connection with connection string
+library(odbc)
+library(getPass)
+
+# Set up connection without DSN
 db <- DBI::dbConnect(odbc::odbc(),
                      Driver = "PostgreSQL Unicode",
                      Database = "postgres",
-                     UserName = "postgres",
-                     Password = getPass("Enter Password:"),
                      Servername = "localhost",
-                     Port = 5432)
+                     Port = 5432,
+                     UserName = "postgres",
+                     Password = getPass("Enter Password:"))
 
 # Set up connection with DSN
 # - Modify /etc/odbc.ini 
 db <- dbConnect(odbc::odbc(), "PostgreSQL")
 
+# Execute SQL query
 DBI::dbGetQuery(db,"SELECT * FROM MTCARS")
 
-
+#Disconnect from database
 DBI::dbDisconnect(db)
 
 
 ### RJDBC setup ###
-# install.packages('RJDBC')
-library(RJDBC)
 
+# # Install Java Client
+# apt-get install default-jdk
+#
+# # Download PostgreSQL JDBC driver
+
+# install.packages('RJDBC')
+
+library(RJDBC)
+library(getPass)
+
+# Specify driver and path to driver in RJDBC 
 db <- DBI::dbConnect(RJDBC::JDBC("org.postgresql.Driver","postgresql-42.2.2.jar"),
                url = "jdbc:postgresql://localhost:5432/postgres",
                user = "postgres",
                password = getPass("Enter Password:"))
 
+# Execute SQL query
 DBI::dbGetQuery(db,"SELECT * FROM MTCARS")
 
+#Disconnect from database
 DBI::dbDisconnect(db)
--- sqlserver-windows.R
+++ sqlserver-windows.R
@@ -1,4 +1,6 @@
-# install.packages(c('odbc','dplyr'))
+### odbc setup ###
+
+# install.packages('odbc')
 
 library(odbc)
 library(getPass)
@@ -8,6 +10,7 @@ library(dbplyr)
 db <- DBI::dbConnect(odbc::odbc(), "SQL")
 
 # Connect without a DSN
+# Configure DSN with ODBC Data Source Administrator (64-bit)
 db <- DBI::dbConnect(odbc::odbc(),
                      Driver = 'ODBC Driver 13 for SQL Server',
                      Server = 'DB-DEV',
@@ -23,3 +26,27 @@ dbGetQuery(db,"SELECT * FROM IRIS")
 smry <- tbl(db,"iris") %>% collect
 
 dbDisconnect(db)
+
+
+### RJDBC setup ###
+
+# # Install Java Client
+#
+# # Download MS SQL Server JDBC driver
+
+# install.packages('RJDBC')
+
+library(RJDBC)
+library(getPass)
+
+# Specify driver and path to driver in RJDBC 
+db <- DBI::dbConnect(RJDBC::JDBC("org.postgresql.Driver","postgresql-42.2.2.jar"),
+                     url = "jdbc:postgresql://localhost:5432/postgres",
+                     user = "postgres",
+                     password = getPass("Enter Password:"))
+
+# Execute SQL query
+DBI::dbGetQuery(db,"SELECT * FROM MTCARS")
+
+#Disconnect from database
+DBI::dbDisconnect(db)
